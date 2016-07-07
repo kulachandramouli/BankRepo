2500065121f1bce71e1a17ea6be098dfeb18e64e
Training <Training@DIN39000563.corp.capgemini.com>
2016-07-07 14:33:02 +0530

    1st commit

--- BankApp/src/Account.java
+++ BankApp/src/Account.java
@@ -1,7 +1,13 @@
 
 public class Account {
-
+	
+	int[] arr={1,8,7,9};
+	
 	public void deposite(){
 		System.out.println("deposite method");
+		
+		for(int i=0;i<arr.length;i++){
+			System.out.println("Data");
+		}
 	}
 }
