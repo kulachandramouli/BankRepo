e86d1468bec1ebd35e2a9ec1d74316ba6d761ee7
Training <Training@DIN39000563.corp.capgemini.com>
2016-07-07 15:19:14 +0530

    fixcase commit

--- BankApp/src/Account.java
+++ BankApp/src/Account.java
@@ -3,6 +3,10 @@ public class Account {
 	
 	int[] arr={1,8,7,9};
 	
+	public void add(){
+		System.out.println("add method added");
+	}
+	
 	public void deposite(){
 		System.out.println("deposite method");
 		
