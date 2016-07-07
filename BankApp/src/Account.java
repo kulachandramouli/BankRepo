a4788dd2adc1b4f0222934686c653a771e91b05a
Training <Training@DIN39000563.corp.capgemini.com>
2016-07-07 14:07:32 +0530

    commiting

---
+++ BankApp/src/Account.java
@@ -0,0 +1,7 @@
+
+public class Account {
+
+	public void deposite(){
+		System.out.println("deposite method");
+	}
+}
---
+++ BankApp/src/MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Account account = new Account();
+		account.deposite();
+	}
+	public static void greetings(){
+		System.out.println("Your Bank operations are here");
+		System.out.println("Wel come to all Customers!!!!");
+	}
+}
